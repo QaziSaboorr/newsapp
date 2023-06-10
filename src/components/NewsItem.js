8e8eef823642263b01dc31f7d2abbfd54819aadc
QaziSaboorr <qazi.ali@ucalgary.ca>
2023-06-10 16:36:21 -0600

    some changes

---
+++ src/components/NewsItem.js
@@ -0,0 +1,35 @@
+import React, { Component } from "react";
+
+export class NewsItem extends Component {
+  render() {
+    let { title, description, imageUrl, newsUrl } = this.props;
+    return (
+      <div className="my-3">
+        <div className="card" style={{ width: "18rem" }}>
+          <img
+            src={
+              imageUrl
+                ? imageUrl
+                : "https://s.yimg.com/ny/api/res/1.2/1DDaSCwVW0jkwi0wVY_xOw--/YXBwaWQ9aGlnaGxhbmRlcjt3PTEyMDA7aD04MDA-/https://s.yimg.com/os/creatr-uploaded-images/2023-06/af0036c0-06b8-11ee-bfda-5ba642c17c61"
+            }
+            className="card-img-top"
+            alt="..."
+          />
+          <div className="card-body">
+            <h5 className="card-title">{title}...</h5>
+            <p className="card-text">{description}...</p>
+            <a
+              href={`${newsUrl}`}
+              target="_blank"
+              className="btn btn-sm btn-primary"
+            >
+              Read More
+            </a>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default NewsItem;
